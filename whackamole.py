fd7d69f84169299e20f47dea558cf4bfd9cbf8b3
robertuan <robertomnez2006@gmail.com>
2024-11-15 17:06:57 -0500

    added clicking detection and mole position changing to random spot on the grid

--- whackamole.py
+++ whackamole.py
@@ -7,7 +7,8 @@ def main():
         # You can draw the mole with this snippet:
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
-
+        pygame.display.set_caption('Molly the Mole')
+        pygame.display.set_icon(pygame.image.load('erobb big ahh head.png'))
         x,y =0, 0
         clock = pygame.time.Clock()
         running = True
@@ -24,10 +25,10 @@ def main():
             screen.fill((255, 193, 204))
             #vertical lines
             for i in range(0,640,32):
-                pygame.draw.line(screen, "black", (i, 0), (i, 512))
+                pygame.draw.line(screen, (112, 122, 17), (i, 0), (i, 512))
             #horizontal lines
             for i in range(0,512,32):
-                pygame.draw.line(screen, "black", (0, i), (640, i))
+                pygame.draw.line(screen, (112, 122, 17), (0, i), (640, i))
             screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
             pygame.display.flip()
             clock.tick(60)
